1ff1badf4e75213d0eb35ccb4eb892e1d44dbb2d
vireshh22 <viresh0489@gmail.com>
2024-07-21 17:07:01 -0400

    queRomanToInt

---
+++ String/RomanToInteger.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.HashMap;
+
+public class RomanToInteger{
+    public static void main(String[] args) {
+        System.out.println("Roman To Integer: ");
+        String str="III";
+        HashMap<Character,Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M', 1000);
+
+        int result=0;
+        for(int i=0;i<str.length();i++){
+            if(i>0 && str.charAt(i)>str.charAt(i-1)){
+                result+=map.get(str.charAt(i))-2*map.get(str.charAt(i-1));
+            }
+            else{
+                result+=map.get(str.charAt(i));
+            }
+        }
+        System.out.println(result);
+    }
+}
