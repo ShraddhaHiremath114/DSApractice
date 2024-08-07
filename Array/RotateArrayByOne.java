dc51a0bc3f87f1d7f6914c00e81a66b8b9620823
ShraddhaHiremath114 <shraddhahiremath114@gmail.com>
2024-08-07 01:25:34 -0400

    RotateByOne

---
+++ Array/RotateArrayByOne.java
@@ -0,0 +1,12 @@
+public class RotateArrayByOne {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int n=arr.length;
+        int last=arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=last;
+        for(int val:arr)System.out.print(val+" ");
+    }
+}
